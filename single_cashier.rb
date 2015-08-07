48498df11c726b1810f3e513a0d004b86938d31a
Shimoyama, Hiroyasu <shimoyama@spicelife.jp>
2015-08-07 20:55:24 +0900

    Add AREA_TAXES hash table.

--- single_cashier.rb
+++ single_cashier.rb
@@ -1,4 +1,13 @@
 class SingleCashier
+
+  AREA_TAXES = {
+    "北海道" => 1.0685,
+    "東日本" => 1.08,
+    "西日本" => 1.0625,
+    "四国"   => 1.04,
+    "九州"   => 1.0825,
+  }
+
   def self.calcurate(price:, number:, region: )
     tax_rate = 1.08
     price.to_i * number.to_i * tax_rate
