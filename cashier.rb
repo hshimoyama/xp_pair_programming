30e80e2ae777fea1a54a925639ac14ee11499101
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-08-07 20:47:33 +0900

    single_cashier return price

--- cashier.rb
+++ cashier.rb
@@ -2,4 +2,4 @@ require './single_cashier'
 p price = ARGV[0]
 p number = ARGV[1]
 p region = ARGV[2]
-SingleCashier.calcurate(price: price, number: number, region: region)
+p SingleCashier.calcurate(price: price, number: number, region: region)
--- single_cashier.rb
+++ single_cashier.rb
@@ -1,6 +1,5 @@
 class SingleCashier
   def self.calcurate(price:, number:, region: )
-    puts price
-    #puts "￥1000"
+    price
   end
 end
