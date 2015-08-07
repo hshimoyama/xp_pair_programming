77ef403caa3c15a052b2de74f439fbb46ac44b1d
Shimoyama, Hiroyasu <shimoyama@spicelife.jp>
2015-08-07 21:12:58 +0900

    Add DISCOUNT_RATES hash table.

--- multiple_cashier.rb
+++ multiple_cashier.rb
@@ -1,4 +1,13 @@
 class MultipleCashier
+
+  DISCOUNT_RATES = {
+    1000 => 0.97,
+    5000 => 0.95,
+    7000 => 0.93,
+    10000 => 0.9,
+    50000 => 0.85,
+  }
+
   def self.calcurate(inputs)
     inputs.inject(0) do |r,input|
       r + SingleCashier.calcurate(
